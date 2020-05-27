6ed964a5e811b0674cf82cfe6130384d3cc1b0e1
Matteo Locatelli <matteo.locatelli96@gmail.com>
2020-05-27 19:20:40 +0200

    Implemented getReviews and getWatchNext

--- serverless-lambda/handler.js
+++ serverless-lambda/handler.js
@@ -54,20 +54,87 @@ module.exports.getWatchNext = async (event, context) => {
   context.callbackWaitsForEmptyEventLoop = false;
   await db();
 
-  return {
-    statusCode: 200,
-    body: JSON.stringify({ message: 'TODO' }),
-  };
+  let body = {}
+    if (event.body) {
+        body = JSON.parse(event.body)
+    }
+    //BAD REQUEST
+    if(!body.id_video) {
+        callback(null, {
+                    statusCode: 400,
+                    headers: { 'Content-Type': 'text/plain' },
+                    body: 'Could not fetch the next talks. No id is provided.'
+        })
+    }
+    
+    
+    console.log('=> get next talks');
+    talk.aggregate([
+      {
+          '$lookup': {
+              'from': 'tedz_data', 
+              'localField': 'watch_next_ids', 
+              'foreignField': '_id', 
+              'as': 'watch_next_info'
+          }
+      }, {
+          '$match': {
+              '_id': body.id_video
+          }
+      }, {
+          '$project': {
+              '_id': 1, 
+              'watch_next_info': 1
+          }
+      }
+    ])
+    .then(talk => {
+        callback(null, {
+            statusCode: 200,
+            body: JSON.stringify(watch_next)
+        })
+      }
+    )
+    .catch(err =>
+        callback(null, {
+            statusCode: err.statusCode || 500,
+            headers: { 'Content-Type': 'text/plain' },
+            body: 'Could not fetch next talks.'
+        })
+    );
 };
 
 module.exports.getReviews = async (event, context) => {
   context.callbackWaitsForEmptyEventLoop = false;
   await db();
 
-  return {
-    statusCode: 200,
-    body: JSON.stringify({ message: 'TODO' }),
-  };
+  let body=event.body;
+  if (!body.id_talk)
+    callback(null, {
+      statusCode: err.statusCode || 400,
+      headers: { 'Content-Type': 'text/plain' },
+      body: 'No id has been provided.',
+    })
+  
+  review
+      .find({ tags: body.id_talk })
+      .sort({ rateAverage: -1 }) // sort by rateAverage
+      .skip(body.rev_per_page * body.page - body.rev_per_page)
+      .limit(body.rev_per_page)
+      .then((reviews) => {
+        callback(null, {
+          statusCode: 200,
+          body: JSON.stringify(reviews),
+        });
+      })
+      .catch((err) =>
+        callback(null, {
+          statusCode: err.statusCode || 500,
+          headers: { 'Content-Type': 'text/plain' },
+          body: 'Could not fetch the reviews.',
+        })
+      );
+  });
 };
 
 module.exports.publishReview = async (event, context) => {
